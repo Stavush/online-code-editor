14ef5601c07ab3e24587d4d33210fb1e8a40ef31
Stavush <stavbenyamin@gmail.com>
2022-12-22 09:54:26 +0200

    fix: .env bug fixes

--- client/src/clients/index.js
+++ client/src/clients/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-require("dotenv").config();
+//require("dotenv").config();
 
 export const backendClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
+  //baseURL: "https://online-code-editor-y1hy.onrender.com/api",
 });
